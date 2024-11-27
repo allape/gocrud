7f911dd204e66a862a73c112a608d326e055c69c
as <allensnape@gmail.com>
2024-11-27 15:03:57 +0800

    chore: refine structure

--- rest.go
+++ rest.go
@@ -1,9 +1,15 @@
 package gocrud
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
+	"net/url"
+	"strings"
 )
+
+var RestCoder = NewDefaultCoder()
 
 type R[T any] struct {
 	Code    Code   `json:"c"`
@@ -42,4 +48,73 @@ func RecoveryHandler(responseFullError bool) gin.HandlerFunc {
 	})
 }
 
-var RestCoder = NewDefaultCoder()
+// SortBy
+// `"id": SortBy("id")` with `?id=asc`                        -> SELECT ... FROM ... ORDER BY `id` ASC
+// `"createdAt": SortBy("created_at")` with `?createdAt=desc` -> SELECT ... FROM ... ORDER BY `created_at` DESC
+func SortBy(name string) SearchHandler {
+	return func(db *gorm.DB, values []string, _ url.Values) {
+		if ok, value := ValuableStringFromArray(values); ok {
+			sort := "asc"
+			if strings.ToLower(value) == "desc" {
+				sort = "desc"
+			}
+			db.Order(fmt.Sprintf("`%s` %s", name, sort))
+		}
+	}
+}
+
+// KeywordIn
+// `"id": KeywordIn("id", nil)` with `?id=1,2,3` -> SELECT ... FROM ... WHERE `id` in (1, 2, 3)
+func KeywordIn(name string, filterFunc func(value []string) []string) SearchHandler {
+	return func(db *gorm.DB, values []string, _ url.Values) {
+		if len(values) == 0 {
+			return
+		}
+
+		if filterFunc != nil {
+			values = filterFunc(values)
+		}
+
+		if len(values) == 0 {
+			return
+		}
+
+		db.Where(
+			fmt.Sprintf("`%s` in ?", name),
+			values,
+		)
+	}
+}
+
+// KeywordLike
+// `"name": KeywordLike("name")` with `?name=foo` -> SELECT ... FROM ... WHERE `name` like '%foo%'
+func KeywordLike(name string, filterFunc func(value string) string) SearchHandler {
+	return func(db *gorm.DB, values []string, _ url.Values) {
+		if ok, value := ValuableStringFromArray(values); ok {
+			if filterFunc != nil {
+				value = filterFunc(value)
+			}
+			db.Where(
+				fmt.Sprintf("`%s` like ?", name),
+				fmt.Sprintf("%%%s%%", value),
+			)
+		}
+	}
+}
+
+// KeywordEqual
+// `"id": KeywordEqual("id")` with `?id=1` -> SELECT ... FROM ... WHERE `id` = 1
+func KeywordEqual(name string, filterFunc func(value string) any) SearchHandler {
+	return func(db *gorm.DB, values []string, _ url.Values) {
+		if ok, value := ValuableStringFromArray(values); ok {
+			var anyValue any = value
+			if filterFunc != nil {
+				anyValue = filterFunc(value)
+			}
+			db.Where(
+				fmt.Sprintf("`%s` = ?", name),
+				anyValue,
+			)
+		}
+	}
+}
